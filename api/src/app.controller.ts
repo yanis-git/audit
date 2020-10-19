a60b7eff8d267889cfafc855e3dcfb89d592aa37
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2020-10-19 22:21:34 +0200

    feat: add performance score

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,12 +1,27 @@
 import { Controller, Get, Query } from '@nestjs/common';
+import * as lighthouse from 'lighthouse';
+import * as chromeLauncher from 'chrome-launcher';
 
 @Controller()
 export class AppController {
   constructor() {}
 
   @Get()
-  audit(@Query('url') url: string): string {
+  async audit(@Query('url') url: string): Promise<any> {
     console.log('auditing ' + url);
-    return url;
+    const chrome = await chromeLauncher.launch({ chromeFlags: ['--headless'] });
+    const options = {
+      onlyCategories: ['performance'],
+      port: chrome.port,
+    };
+    const runnerResult = await lighthouse(url, options);
+
+    await chrome.kill();
+    return {
+      url,
+      performance: {
+        score: runnerResult.lhr.categories.performance.score * 100,
+      },
+    };
   }
 }

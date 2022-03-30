0b8326f1c5ecab599f54d90b91bf7169145cb67e
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2022-03-30 11:33:00 +0200

    fix: solve issue with checkIfLandmarks

--- src/rules/checkIfLandmarks.ts
+++ src/rules/checkIfLandmarks.ts
@@ -10,10 +10,10 @@ export type CheckIfLandmarksAuditResult = AuditResult & {
     }
 }
 export const checkIfLandmarks: AuditFunction = async (page: Page): Promise<CheckIfLandmarksAuditResult | false> => {
-    const hasHeaderLandmark = await page.evaluate(() => Array.from(document.querySelectorAll("header")).length === 0) as boolean;
-    const hasNavLandmark = await page.evaluate(() => Array.from(document.querySelectorAll("nav")).length === 0) as boolean;
-    const hasMainLandmark = await page.evaluate(() => Array.from(document.querySelectorAll("main")).length === 0) as boolean;
-    const hasFooterLandmark = await page.evaluate(() => Array.from(document.querySelectorAll("footer")).length === 0) as boolean;
+    const hasHeaderLandmark = await page.evaluate(() => Array.from(document.querySelectorAll("header")).length > 0) as boolean;
+    const hasNavLandmark = await page.evaluate(() => Array.from(document.querySelectorAll("nav")).length > 0) as boolean;
+    const hasMainLandmark = await page.evaluate(() => Array.from(document.querySelectorAll("main")).length > 0) as boolean;
+    const hasFooterLandmark = await page.evaluate(() => Array.from(document.querySelectorAll("footer")).length > 0) as boolean;
 
     if (hasHeaderLandmark && hasNavLandmark && hasMainLandmark && hasFooterLandmark) {
         return false;
